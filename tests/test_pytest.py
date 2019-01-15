66d71cf2dc7692fb7fd06ec82bea61501e0a9d2e
Dave Keeshan <dkeeshan@gmail.com>
2019-01-15 22:34:20 +0000

    Fixed version ids for import_lwo

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -6,11 +6,11 @@ def bpy_module(cache):
     return(cache.get('bpy_module', None))
     
 def test_versionID_pass(bpy_module):
-    expect_version = (0, 0, 1)
+    expect_version = (1, 2)
     return_version = get_version(bpy_module)
     assert  expect_version == return_version
     
 def test_versionID_fail(bpy_module):
-    expect_version = (0, 1, 1)
+    expect_version = (2, 2)
     return_version = get_version(bpy_module)
     assert  not expect_version == return_version
