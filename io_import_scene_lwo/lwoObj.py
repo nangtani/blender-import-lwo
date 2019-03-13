4023995be499553000d4ff8301e01a3500cb8f88
Dave Keeshan <dkeeshan@gmail.com>
2019-03-13 19:32:54 +0000

    Add some debug

--- io_import_scene_lwo/lwoObj.py
+++ io_import_scene_lwo/lwoObj.py
@@ -1073,8 +1073,7 @@ class lwoObj(object):
                         self.images.append(ifile)
                     continue
             if None == ifile and not self.allow_missing_images:
-                #raise lwoNoImageFoundException(f"No valid image found for path: {orig_path}")
-                raise lwoNoImageFoundException("No valid image found for path: {} {} {}".format(orig_path, imagefile, dirpath))
+                raise lwoNoImageFoundException("No valid image found for path: {} {} {} {}".format(orig_path, imagefile, dirpath, files))
         
             os.chdir(cwd)
             self.clips[c_id]['new_path'] = ifile
