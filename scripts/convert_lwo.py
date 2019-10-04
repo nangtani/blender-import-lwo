dab2473e1c513b6ade1a0b40bace5c1530a1414f
Dave Keeshan <dave.keeshan@movidius.com>
2019-10-04 13:24:29 +0100

    Change file

--- scripts/convert_lwo.py
+++ scripts/convert_lwo.py
@@ -27,18 +27,19 @@ def main(infile):
 
 
 if __name__ == "__main__":
-    #infile = "tests/basic/src/box/box1.lwo"
+    infile = "tests/basic/src/box/box1.lwo"
     #infile = "tests/basic/src/box/box1-uv.lwo"
-    infile = "tests/basic/src/LWO2/box/box2-uv.lwo"
-    infile = "tests/basic/src/LWO2/box/box3-uv-layers.lwo"
+    #infile = "tests/basic/src/LWO2/box/box2-uv.lwo"
+    #infile = "tests/basic/src/LWO2/box/box3-uv-layers.lwo"
     #infile = "tests/basic/src/LWO/box/box3-uv-layers.lwo"
     #infile = "tests/basic/src/LWO2/box/box4-uv-layers.lwo"
     #infile = "tests/basic/src/LWO2/box/box5-ngon.lwo"
     #infile = "tests/basic/src/LWO2/src/Federation - Phobos/objects/USS-Phobos.lwo"
-    infile = "tests/lwo_phobos/src/LWO2/Federation - Phobos/objects/USS-Phobos.lwo"
+    #infile = "tests/lwo_phobos/src/LWO2/Federation - Phobos/objects/USS-Phobos.lwo"
     #infile = "src/Federation - Phobos/objects/USS-Phobos.11.5.lwo"
-    #infile = "tests/interceptor/src/LWO2/Federation - Interceptor/objects/interceptor_hull.lwo"
+    infile = "tests/lwo_interceptor/src/LWO2/Federation - Interceptor/objects/interceptor_hull.lwo"
     #infile = "tests/basic/src/LWO2/box/box3-uv-layers.lwo"
     #infile = "tests/basic/src/LWO3/box/box3-uv-layers.lwo"
     #infile = "tests/basic/src/LWO2/ISS models 2011/Objects/Modules/columbus/columbus.lwo"
+    #infile = "tests/lwo_phobos/src/LWO2/Federation - Phobos/objects/USS-Phobos.lwo"
     main(infile)
