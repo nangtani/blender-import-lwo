a28e3ae985ff5a67fa7c31a274d39e7c26c51f23
Dave Keeshan <dkeeshan@gmail.com>
2020-05-11 00:13:19 +0100

    Added parsing of the texture location

--- io_scene_lwo/bpy_debug.py
+++ io_scene_lwo/bpy_debug.py
@@ -1,2 +1,11 @@
-class DebugException(Exception):
-    pass
+BPY_DEBUG = 1
+
+class DebugException:
+    def __init__(self, msg, debug=BPY_DEBUG):
+        
+        if 0 == BPY_DEBUG:
+            return
+        if 1 == debug:
+            print(msg)
+        elif 2 == debug:
+            raise Exception(msg)
